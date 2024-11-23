9f380bd7f9630b3cdcc1c4faf9f17a0ebaa5786e
mistr-7 <mistarseven581@gmail.com>
2024-11-23 10:47:12 +0000

    some trans

--- mappings.py
+++ mappings.py
@@ -1,4 +1,23 @@
-weather_maps = {
-    "abc" : ["xyz", "pqr"]
+commodity_mapping = {
+    'RICE': ['Rice'],
+    'WHEAT': ['Wheat'],
+    'KHARIF SORGHUM': ['Sorghum'],
+    'RABI SORGHUM': ['Sorghum'],
+    'SORGHUM': ['Sorghum'],
+    'PEARL MILLET': ['Pearl Millet'],
+    'MAIZE': ['Maize'],
+    'FINGER MILLET': ['Finger Millet'],
+    'BARLEY': ['Barley'],
+    'CHICKPEA': ['Chickpea'],
+    'PIGEONPEA': ['Pigeonpea'],
+    'MINOR PULSES': ['Minor Pulses'],
+    'PULSES': ['Pulses'],
+    'GROUNDNUT': ['Groundnut'],
+    'SESAMUM': ['Sesamum'],
+    'LINSEED': ['Linseed'],
+    'SUGARCANE': ['Sugarcane'],
+    'COTTON': ['Cotton'],
+    'FRUITS AND VEGETABLES': ['Fruits and Vegetables'],
+    'FODDER': ['Fodder']
 }
 # isme sare mapping dalte rehna. also naams simple rakhio like rainfail_cm, etc..
